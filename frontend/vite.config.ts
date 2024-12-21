7ecd2e2a286177a85b0a50e9768f8c0ace241345
João Saraiva <j.pedroabrantessaraiva@gmail.com>
2024-12-21 15:26:49 +0000

    working on the docker image for the backend and frontend apps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
+  preview: {
+    host: true,
+    port: 5173,
+  },
   server: {
-    host: "0.0.0.0",
-    port: 3000,
+    host: true,
+    port: 5173,
   },
   plugins: [react()],
 });
